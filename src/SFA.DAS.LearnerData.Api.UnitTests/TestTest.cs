1c7c9356451f4e0374d8699d7f0bc5b3551378ff
Jon Harris <jon.harris@fuitsu.com>
2025-03-13 11:37:01 +0000

    A couple of testing tests

---
+++ src/SFA.DAS.LearnerData.Api.UnitTests/TestTest.cs
@@ -0,0 +1,14 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace SFA.DAS.LearnerData.Api.UnitTests;
+
+public class TestTest
+{
+    // TODO remove after build pipelines are running ok
+    [Test]
+    public void ThisIsATest()
+    {
+        1.Should().Be(1);
+    }
+}
