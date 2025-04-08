7bf6204c0f5229a790632e672ebcbc90e8cea1aa
Jon Harris <jon.harris@fuitsu.com>
2025-04-08 11:51:34 +0100

    Adding controller test

---
+++ src/SFA.DAS.LearnerData.Api.UnitTests/Controllers/LearnersController/WhenICreateLearner.cs
@@ -0,0 +1,24 @@
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using SFA.DAS.LearnerData.Api.Models.Requests;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.LearnerData.Api.UnitTests.Controllers.LearnersController;
+
+public class WhenICreateLearner
+{
+    [Test, MoqAutoData]
+    public async Task Then_Created_Result_Is_Returned(
+        CreateLearnerRequest request,
+        [Greedy] Api.Controllers.LearnersController sut
+        )
+    {
+        var result = await sut.Create(request);
+        result.Should().NotBeNull();
+        
+        var createdResult = result as CreatedResult;
+        createdResult.Should().NotBeNull();
+    }
+}
