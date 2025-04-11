f0f448aa5d9e887236cefbc9864833fb5587d04b
ogrwill <will.ogrady@digital.education.gov.uk>
2025-04-11 16:00:46 +0100

    adding info endpoint. updating launchSettings

---
+++ src/SFA.DAS.LearnerData.Api/Controllers/InfoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.LearnerData.Api.Controllers;
+[Route("api")]
+[ApiController]
+[AllowAnonymous]
+public class InfoController(IConfiguration config) : ControllerBase
+{
+    [HttpGet("info")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult GetInfo()
+    {
+        var info = new
+        {
+            Version = config["ApiVersion"] ?? "1.0.0",
+            Name = "Learner Data API"
+        };
+        return Ok(info);
+    }
+}
