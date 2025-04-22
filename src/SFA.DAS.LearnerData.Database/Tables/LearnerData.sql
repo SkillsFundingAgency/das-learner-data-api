7c1bdf01e6d876a5c4b9fd2557e9fa12522f6e68
Jon Harris <jon.harris@fuitsu.com>
2025-04-22 12:41:00 +0100

    Allowing null for ConsumerReference in the database

--- src/SFA.DAS.LearnerData.Database/Tables/LearnerData.sql
+++ src/SFA.DAS.LearnerData.Database/Tables/LearnerData.sql
@@ -20,7 +20,7 @@ CREATE TABLE [dbo].[LearnerData](
     [UpdatedDate] [datetime] NULL,
     [ReceivedDate] [date] NOT NULL,
     [CorrelationId] [nvarchar](max) NOT NULL,
-    [ConsumerReference] [nvarchar](max) NOT NULL,
+    [ConsumerReference] [nvarchar](max) NULL,
     CONSTRAINT [PK_LearnerData] PRIMARY KEY CLUSTERED
 (
 [Id] ASC
