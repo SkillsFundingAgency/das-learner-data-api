c5a4af0f059c0ad62a4752c2a86e6e07ab64e25d
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2025-07-21 10:19:15 +0100

    remove irrelevant stuff

--- src/SFA.DAS.LearnerData.Messages/LearnerDataUpdatedEvent.cs
+++ src/SFA.DAS.LearnerData.Messages/LearnerDataUpdatedEvent.cs
@@ -1,11 +1,7 @@
-using NServiceBus;
-
 namespace SFA.DAS.LearnerData.Messages;
 
 public class LearnerDataUpdatedEvent
 {
     public long LearnerId { get; set; }
     public ChangeSummary ChangeSummary { get; set; } = new();
-    public string EventSource { get; set; } = "SFA.DAS.LearnerData.Api";
-    public Guid EventId { get; set; } = Guid.NewGuid();
 } 
