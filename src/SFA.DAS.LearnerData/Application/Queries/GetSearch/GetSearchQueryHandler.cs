1393dd6939e14f2874af5fa80d35b0fae7ba0f31
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2026-02-26 13:56:10 +0000

    expose new properties on response

--- src/SFA.DAS.LearnerData/Application/Queries/GetSearch/GetSearchQueryHandler.cs
+++ src/SFA.DAS.LearnerData/Application/Queries/GetSearch/GetSearchQueryHandler.cs
@@ -81,6 +81,9 @@ public class GetSearchQueryHandler(ILearnerRepository repository): IRequestHandl
                 CorrelationId = learner.CorrelationId,
                 ReceivedDate = learner.ReceivedDate,
                 StandardCode = learner.StandardCode,
+                TrainingCode = learner.TrainingCode,
+                TrainingName = learner.TrainingName,
+                LearningType = learner.LearningType,
                 IsFlexiJob = learner.IsFlexiJob,
                 PlannedOTJTrainingHours = learner.PlannedOTJTrainingHours
             }),
