9d5fd87fe77c23ee8ca8e3705c2323ccb26ae793
Witek <witek.poreda@education.gov.uk>
2026-03-19 14:55:54 +0000

    add tests

--- src/SFA.DAS.LearnerData.UnitTests/Repositories/WhenIAssignApprenticeshipId.cs
+++ src/SFA.DAS.LearnerData.UnitTests/Repositories/WhenIAssignApprenticeshipId.cs
@@ -8,6 +8,7 @@ using SFA.DAS.LearnerData.Application.Commands.AssignApprenticeshipId;
 using SFA.DAS.LearnerData.Data;
 using SFA.DAS.LearnerData.Data.Entities;
 using SFA.DAS.LearnerData.Data.Repositories;
+using SFA.DAS.LearnerData.Messages;
 using SFA.DAS.Testing.AutoFixture;
 
 namespace SFA.DAS.LearnerData.UnitTests.Repositories;
@@ -339,6 +340,49 @@ public class WhenIAssignApprenticeshipId
         await _dbContext.SaveChangesAsync();
 
         var response = await _repository.Search(providerId, 1, 10, 1000, 0, null, false, "", false, null, 2025, null, "", courseCode, null, _cancellationToken);
+        var results = response.Data.ToList();
+        results.Count.Should().Be(0);
+    }
+
+    [Test, MoqAutoData]
+    public async Task When_Learner_Search_Should_Include_Learners_When_LearningType_Matches(
+       AssignApprenticeshipIdCommand command,
+       long providerId,
+       LearningType learningType,
+       List<Learner> learners)
+    {
+        learners.ForEach(x =>
+        {
+            x.Ukprn = providerId;
+            x.StartDate = new DateTime(2025, 05, 01);
+            x.LearningType = learningType;
+        });
+
+        _dbContext.Learners.AddRange(learners);
+        await _dbContext.SaveChangesAsync();
+
+        var response = await _repository.Search(providerId, 1, 10, 1000, 0, null, false, "", false, null, 2025, null, "", null, learningType, _cancellationToken);
+        var results = response.Data.ToList();
+        results.Count.Should().Be(learners.Count);
+    }
+
+    [Test, MoqAutoData]
+    public async Task When_Learner_Search_Should_Not_Include_Learners_When_LearningType_Not_Matches(
+       AssignApprenticeshipIdCommand command,
+       long providerId,
+       List<Learner> learners)
+    {
+        learners.ForEach(x =>
+        {
+            x.Ukprn = providerId;
+            x.StartDate = new DateTime(2025, 05, 01);
+            x.LearningType = LearningType.Apprenticeship;
+        });
+
+        _dbContext.Learners.AddRange(learners);
+        await _dbContext.SaveChangesAsync();
+
+        var response = await _repository.Search(providerId, 1, 10, 1000, 0, null, false, "", false, null, 2025, null, "", null, LearningType.ApprenticeshipUnit, _cancellationToken);
         var results = response.Data.ToList();
         results.Count.Should().Be(0);
     }
